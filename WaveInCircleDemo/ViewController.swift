c03bb16c019fc6ae700827d8ee7772cf2378bf8c
Owen Ju <owenju@users.noreply.github.com>
2017-08-05 15:19:54 +0800

    Initial Commit

---
+++ WaveInCircleDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WaveInCircleDemo
+//
+//  Created by 鞠汶成 on 05/08/2017.
+//  Copyright © 2017 鞠汶成. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
